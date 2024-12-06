a2862d6cd4ab7fe93b7f3b6047cc28176613e88d
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2024-12-06 11:06:13 -0600

    Update OrganizedConfusion.py

--- OrganizedConfusion.py
+++ OrganizedConfusion.py
@@ -1,1 +1,27 @@
+i = 0
+while i < 3:
+    print("Meow")
+    i += 1
 
+#in C wewould also do this:
+
+for (int i = 0: i <3; i++)
+{
+    printf("Meow"\n");
+    }
+
+    #python this code would be annoying / wrap on itself / you have to write out ",1" ",2"
+
+    for i in [0, 1, 2]:
+      print("hello, world")
+
+    #Alternative
+
+    for i in range(3):
+        Print("hello, world")
+
+
+  OC = Organized Confusion
+  for "My Name" print("Martian")
+
+  
