31e8db6551c7853b2b9486b67c6b44b26e56c917
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2024-12-08 11:14:04 -0600

    Update OrganizedConfusion.py

--- OrganizedConfusion.py
+++ OrganizedConfusion.py
@@ -1,27 +1,36 @@
-i = 0
-while i < 3:
-    print("Meow")
-    i += 1
+s = input("Do you agree? ")
+if s == "Y" or s == "y":
+    print("Agreed")
+elif s =="N" or s == "n":
+    print("Not Agreed")
 
-#in C wewould also do this:
+#Alternatively / Express the same Idea via
 
-for (int i = 0: i <3; i++)
-{
-    printf("Meow"\n");
-    }
+s = input("Do you still agree? ")
 
-    #python this code would be annoying / wrap on itself / you have to write out ",1" ",2"
+#if the answer is in the []list provided print Agreed.
 
-    for i in [0, 1, 2]:
-      print("hello, world")
+if s in ["Y", "y"]:
+    print("Agreed")
 
-    #Alternative
+#if the asnwer in NOT in the []list provided print Not Agreed.
 
-    for i in range(3):
-        Print("hello, world")
+elif s in ["N", "n"]:
+    print("Not Agreed")
 
+#Use s.lower method to accept any variation of your response regarding Upper & lower via Object Oriented Programming.
 
-  OC = Organized Confusion
-  for "My Name" print("Martian")
+s = input("Do you still agree now? ")
+if s.lower() in ["y", "yes"]:
+    print("You're Persistant.")
+elif s.lower() in ["n", "no"]:
+    print("finally you disagree.")
 
-  
+#We can remove the redundancy of 2 s.lower(s) via changing the var "s" to return lower case via.
+
+s = input("what about now? ")
+s = s.lower()
+if s in ["y", "yes"]:
+    print("You're Persistant.")
+elif s in ["n", "no"]:
+    print("You really don't disagree?")
