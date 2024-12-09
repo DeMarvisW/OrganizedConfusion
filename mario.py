c63aee6fe858fc24fde17bda8496e592a1d70171
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2024-12-09 15:26:40 -0600

    Create mario.py
    define and call a function

---
+++ mario.py
@@ -0,0 +1,17 @@
+#defined a function named main() including what it should do once called upon.
+def main():
+    height = get_height()
+    for i in range(height):
+        print("#")
+
+#Defined the parameters within that function and what action they take when called upon ex: get_height()
+def get_height():
+    while True:
+        try:
+            n = int(input("Height: "))
+            if n > 0:
+                return n
+        except ValueError:
+            print("Not an integer")
+#Call the function that has been defined so that when the code is excuted the parameters are enabled.
+main()
