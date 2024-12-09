70938d9c4f8b1bdaf63cf7e148b9090be39df5d2
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2024-12-09 16:53:22 -0600

    Update score.py
    #scores is equal to an empty list, a list with 3 entrys, Define score as a number input when prompted by the string titled “Scores:". The numbers that are input when prompted, are added to the empty list named “scores” via the “.append” feature. Define “average” as the SUM of the numbers entered in the “scores” list devided by the LENGTH (amount) of the numbers added to the scores list. 
    
    With this instruction and specification it is to PRINT a Function named “Average:" displaying the function previously defined as “average” (The sum of the scores divided by the amount of the scores provided, 3 in this case due to the range of 3).

--- score.py
+++ score.py
@@ -2,7 +2,7 @@ scores = []
 for i in range(3):
     score = int(input("Scores: "))
     scores.append(score)
-
+#rather than .append you may use "scores += [score]" to add to the empty list titled "scores".
 
 average = sum(scores) / len(scores)
 print(f"Average: {average}")
