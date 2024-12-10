0859d3048f44083d899cea8333d11faa9a9e8853
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2024-12-09 18:41:19 -0600

    Update names.py
    #1. import system library
    
    #3. Create Varable titled “names” containing a list of names.
    
    #5. Create a Variable titled “name” prompting the input of a name via the string “Name: ”
    
    #7. for n in the variable “names”
    #8. if name (==) is indeed equal to n (in the list)
    #9. The system will print “Found”
    #10. Then exit the system library
    
    #12. Otherwise will print “Not Found”
    #13. Then exit the system library
    
    ALTERNATIVLY
    
    We can simplify via removing the For loop:
    
    #Replacing with: “if name in names” 
    
    Meaning that if the variable that we input for “name” is OR isn’t in the “names” list do one or the other (Found OR Not Found).

--- names.py
+++ names.py
@@ -11,3 +11,19 @@ for n in names:
 
 print("Not Found")
 sys.exit(1)
+
+
+#Alternatively we can simplify via removing the for loop via line 7
+
+import sys
+
+names = ["Bill", "Charlie", "Fred", "George", "Ginny", "Percy", "Ron"]
+
+name = input("Name: ")
+
+if name in names:
+        print("Found")
+        sys.exit(0)
+
+print("Not Found")
+sys.exit(1)
