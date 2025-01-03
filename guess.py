a3e0d84bf6bef308ea6d3f54401ee640f9bea1f1
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-03 12:23:54 -0600

    Update guess.py

--- guess.py
+++ guess.py
@@ -2,7 +2,7 @@
 import random
 
 lowest_num = 1
-highest_num = 100
+highest_num = 3
 answer = random.randint(lowest_num, highest_num)
 guesses = 0
 is_running = True
@@ -19,9 +19,17 @@ while is_running:
         guesses += 1
 
         if guess < lowest_num or guess > highest_num:
-            print("That guess is out of range!")
+            print("That number is out of range!")
             print(f"Please select a number between {lowest_num} and {highest_num}:")
-    else:
-        print("Invalid Guess")
-        print(f"Please select a number between {lowest_num} and {highest_num}:")
+        elif guess < answer:
+            print("Too low! Try again.")
+        elif guess > answer:
+            print("Too High! Try again.")
+        else:
+            print(f"CORRECT! The answer was {answer}")
+            print(f"Please select a number between {lowest_num} and {highest_num}:")
+            is_running = False
 
+    else:
+        print("Invalid guess!")
+        print(f"Please Select a number between {lowest_num} and {highest_num}")
