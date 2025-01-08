286f3ae3e1e2fd334c92dab679b987c851b16eb2
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-08 16:06:45 -0600

    Update cpi.py

--- cpi.py
+++ cpi.py
@@ -19,5 +19,9 @@ while time <= 0:
     if time <= 0:
         print("Time can't be less than or equal to Zero!")
 
+# Formula to calc interest, Total balance = principle * 1 + our Rate Divided by 100 enclose function within ().
+# Then enclose that function within the power function and Raise it to the power of time.
+
 total = principle * pow((1 + rate / 100), time)
 print(f"Balance after {time} year/s: ${total:.2f}")
+
