3a216636310edf219b2611581fb082bcf3736485
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-10 17:11:48 -0600

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,47 @@
+# Function =  a block of reusable code
+#             place () after the function's name to invoke it.
+
+# Define a function = "def" + "Create a Name" + parenthesis followed by : 
+# Any code that belongs to function you'll be required to indent underneath.
+
+# def Happy_Birthday(name, age):
+#    print(f"Happy birthday to {name}!")
+#    print(f"You are {age}")
+#    print(f"Happy birthday to {name}!")
+#    print()
+
+# To invoke the function you type the name of the function with parenthesis i.e: Happy_Birthday()
+# Using whats called an argument you can send data directly to a function.
+# via placing a parameter within the function (a temp variable):
+# In order to invoke you need a matching number of arguments. Can't have only one but attempt to invoke two.
+
+# def Happy_Birthday("One", 2):
+#    print("Happy birthday to {name}!")
+#    print("how old are you?")
+#    print("Happy birthday to you!")
+#    print()
+# happy_Birthday("Martian", 35)
+
+# Happy_Birthday("Martian", 35)
+
+
+
+# Create an invoice
+
+# def display_invoice(name, amount, due):
+#     print(f"Hello {name}")
+#     print(f"you bill of ${amount} is due on {due}")
+
+# display_invoice("Martian", 100, "1/2025")
+
+# print() 
+
+#                                    RETURN
+
+# Return Statement = A statement used to enda a fundciton and send a result back to a caller.
+
+def add(x, y):
+    z = z + y
+   return z
+
+def subtract()
