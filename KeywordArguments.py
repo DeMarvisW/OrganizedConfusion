1737effdc1991a54bfe55c211817cb1661289342
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-12 18:30:59 -0600

    Update KeywordArguments.py

--- KeywordArguments.py
+++ KeywordArguments.py
@@ -15,10 +15,24 @@
 
 # Next let's print the numbers 1 - 10 using a "For Loop":
 
-for x in range(1, 11): 
-    print(x)            # After each print statement we print a new line meaning each number will be displayed on it's own line.
+# for x in range(1, 11): 
+#     print(x)            # After each print statement we print a new line meaning each number will be displayed on it's own line.
 #                                    If we would like the numbers displayed on the same line we must follow x w a ',' 'end=" "'
 #                                    Via adding the comma "end with a space", we tell the system NOT to continue via a new line 
 #                                           BUT to contiue on same line following a space instead.
 #     "end" is a "KEYWORD" Argument auto applied to the "print" function that tells the system to go to a new line.
 #      We tell the system NOT to go to a new line but instead end w a space, presenting our output onto the same line.
+
+
+#      Same can be said for the "Separate" Function.
+#      Ex: print("1", "2", "3", "4", "5", "6" sep="*")
+#      We can use the Separate keyword argument 'sep=" "' to separate these strings / numbers by whatever we want. 
+
+# NEXT WE CREATE  A FUNCITON TO GENERATE A PHONE NUMBER.
+
+def get_phone(country, area, first, last):  # Define the function as a set of arguments we input.
+    return f"{country}-{area}-{first}-{last}"   # Set it to return an f string that matches the name of the functions parameters.
+
+phone_num = get_phone(country=1, area=123, first=456, last=7890)
+# ^ Create a variable that invokes your created function (The order doesn't matter when using Keyword arguments).
+print(phone_num)    #When you print. You get the designated numbers in the designated order.
