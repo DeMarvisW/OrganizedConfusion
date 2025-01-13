296300624dedf350c32067a11dc86db8e7dc0c7f
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-12 21:22:01 -0600

    Create ArbitraryArguments.py

---
+++ ArbitraryArguments.py
@@ -0,0 +1,19 @@
+#                                   Arbitrary Arguments JAN 12, 2025
+# Arbitrary = A varying amount of arguments.
+# *args     = Allows you to pass multiple non-key arguments (TUPLE).
+# **kwargs  = Allows you to pass multiple keyword arguments {DICTIONARY}.
+#           * Unpacking operator
+#           1. Positional 2. Default 3 Keyword 4. ARBITRARY
+
+# def print_address(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}:{value}")
+
+# print_address(street="123 Baldwin St",
+#               city="Hutto",
+#               state="Texas",
+#               zip="78634")
+
+# print_address
+
+
