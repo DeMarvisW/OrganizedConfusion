50b9309b6ff4d0e1ac2cdc43a2e60876d920166e
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-16 22:06:26 -0600

    Create Modules.py

---
+++ Modules.py
@@ -0,0 +1,19 @@
+#                                         Modules - Jan 16, 2025
+
+# Module = a file containing code you want to include in your program. Use "IMPORT" to include a module (built-in or your own.)
+#          Useful to break up a large program into reusable seprate files.
+# print(help("modules")) = a list of all the standard modules found within python.
+
+# import math
+# print(math.pi)            Returns the value of pi (3.141592653589793)
+
+# You may give your module an alias or nickname via:
+
+# "Import" math "as" whatever_you_choose
+# print(whatever_you_choose.pi)
+
+# "from" math "import" "pi" (should maybe try to avoid this one due to possible comflicts
+
+# Can create your own modules via creating a new file and importing functions you created in said file via importing the file like that of CS50.
+
+# can utilize the import function to call that file and utilize any functions previously defined in that file.
