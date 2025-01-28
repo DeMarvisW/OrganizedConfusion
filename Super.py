0a7ecf806ee92e5d522a050718705b68e921d91c
DeMarvisW <37967079+DeMarvisW@users.noreply.github.com>
2025-01-28 10:44:51 -0600

    Update Super.py

--- Super.py
+++ Super.py
@@ -12,26 +12,42 @@ class Shape:
     def __init__(self, color, is_filled):                   # Constuctor filled with Attributes for the class "Shape".
         self.color = color                                  # Attributes are defined as Actions, Characteristics, & functions for the class "Shape"
         self.is_filled = is_filled                          # They're assigned as a counter part of the Dunder Method "def __init__". Each attribute within the Tuple must be accompanied by an action via "self." and the equal "=" sign to complete the method (line 12 - 14). 
-        
+
+def describe(self):
+    print(F"It is {self.color} and {'filled' if self.is_filled else 'not filled'}")
 
 class Circle(Shape):
     def __init__(self, color, is_filled, radius):
-        super(Shape).__init_(color, is_filled)
+        super().__init_(color, is_filled)
         self.radius = radius
-#                                    We have many differences within these classes. But we use the Super class to fill the 
-class Square(Shape):            #     classes / objects with the attributes they have in common, utilizing less lines of code.
+
+def describe(self):
+    super().describe():
+    print(F"It is a circle with an area of {3.14 * self.radius * self.radius}cm^2")
+    
+
+                                                             # We have many differences within these classes. But we use the Super class to fill the 
+class Square(Shape):                                         # classes / objects with the attributes they have in common, utilizing less lines of code.
     def __init__(self, color, is_filled, width):
-        super(Shape).__init_(color, is_filled)
+        super().__init_(color, is_filled)
         self.width = width
+
+def describe(self):
+    print(F"It is a square with an area of {self.width * self.width}cm^2")
+    super().describe()
 
 class Triangle(Shape):
     def __init__(self, color, is_filled, width, height):
-        super(Shape).__init_(color, is_filled)
+        super().__init_(color, is_filled)
         self.width = width
         self.height = height
-#                                     Moving forward we create Objects from the classes to inherite their attributes. But we also have to provide its own attributes just like above to complete the method / function.
-Circle = Circle("Green", True, 14)  # (color: Any, is_filled: Any, radius: Any) To complete a Circle
-Square = Square("Blue", True, 18)   # We could even user keyword arguments "color="red", is_filled="True" for better readability"
+
+def describe(self):
+    print(F"It is a triangle with an area of {self.height * self.width / 2}cm^2")
+    super().describe()
+                                                               # Moving forward we create Objects from the classes to inherite their attributes. But we also have to provide its own attributes just like above to complete the method / function.
+Circle = Circle("Green", True, 14)                             # (color: Any, is_filled: Any, radius: Any) To complete a Circle
+Square = Square("Blue", True, 18)                              # We could even user keyword arguments "color="red", is_filled="True" for better readability"
 Triangle = Triangle("Gold", True, 20, 44)
 
 print(Triangle.color)
