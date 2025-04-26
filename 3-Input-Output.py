e55beca726b237c6387c5945cc618dd65d594a00
DeMarvis K Williams <37967079+DeMarvisW@users.noreply.github.com>
2025-04-25 21:28:20 -0500

    Update 3-Input-Output.py

--- 3-Input-Output.py
+++ 3-Input-Output.py
@@ -1,1 +1,31 @@
+#name = input("What is your name? " )
+#print("Hello", name)
 
+#print("=================================")
+#age = int(input("Hold old are you? "))      # Must convert variable age to an int so that the next line may calculate w/o error
+#years_to_100 = 100 - age
+#print(f"You will be 100 in {years_to_100} years.")
+
+#print("=================================")
+#num1 = float(input("Enter the first number "))
+#num2 = float(input("Enter the second number "))
+#sum_result = num1 + num2
+#print(f"The sum of {num1} and {num2} is {sum_result}")
+
+#print("=================================")
+# Working with multiple inputs on one line
+#x,y = input("Enter two values separated by a space: ").split()
+#print(f"first value {x}, second value {y}")
+
+#print("=================================")
+#user_choice = input("Choose a color (or press enter for default.): ")
+#if user_choice == "":
+#    user_choice = "Green"
+#print(f"Selected Color: {user_choice}")
+
+print("=================================")
+
+length = float(input("Enter length in meters: "))
+print(f"that's {length * 100} centimeters")
+
+print("=================================")
