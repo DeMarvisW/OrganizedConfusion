b1d89c436e58075c4d8f536080d46679d4a7fd2f
DeMarvis K Williams <37967079+DeMarvisW@users.noreply.github.com>
2025-04-25 23:26:54 -0500

    Update 5-Loops.py

--- 5-Loops.py
+++ 5-Loops.py
@@ -28,19 +28,60 @@
 # Looping through a List
 
 fruits = ["apple", "banana", "cherry"]
-print("My Fruit:")
-for fruit in fruits:
-    print(fruit)
+#print("My Fruit:")
+#for fruit in fruits:
+#    print(fruit)
 
 
 # Looping through a List in reverse
 
-print("\nMy fruits in reverse:")
-for fruit in reversed(fruits):
-    print(fruit)
+#print("\nMy fruits in reverse:")
+#for fruit in reversed(fruits):
+#    print(fruit)
 
 # List enumerated
 
-print("fruit with indices:")
-for index, fruit in enumerate(fruits):
-    print(f"{index}:{fruit}")
+#print("fruit with indices:")
+#for index, fruit in enumerate(fruits):
+#    print(f"{index}:{fruit}")
+
+# Loop with Dictionaries
+
+#person = {"name": "Martian", "age": "36", "city": "Austin"}
+#print("Person Dictionary")
+#for key, value in person.items():
+#    print(f"{key}:{value}")
+
+# List Comprehension (Compact loop for creating List)
+
+#squares = [x**2 for x in range(1,6)]   #[1,4,9,16,25]
+#print("Squares from 1 to 5", squares)
+
+# For Loop with Zip()
+
+#colors = ["red", "yellow","green"]
+#for fruit,color in zip(fruits,colors):
+#    print(f"{fruit} is {color}")
+
+#Break and Continue
+
+#print("Loop with break")
+#for i in range(1,10):
+#    if i > 5:
+#        break
+#    print(i)
+
+#print("\nLoop with Continue")
+#for i in range(1, 10):
+#    if i % 2 == 0:
+#        continue    # skip even numbers
+#    print(i)
+
+# Infinite Loops with break condition
+
+i = 1
+while True:
+    print(i)
+    i += 1
+    if i > 5:
+        break
